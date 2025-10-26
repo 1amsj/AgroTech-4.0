14be300253c5c22aabb081ca4df5de13316941c2
legan33333dhiro <jesus2026bbb@gmail.com>
2025-10-26 19:29:07 -0400

    Update Bootstrap to version 5 and fix carousel caption HTML in login page

--- Vista/login.php
+++ Vista/login.php
@@ -15,12 +15,8 @@
     <link href="https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <!-- Bootstrap icons and Bootstrap 4 stylesheet (using local Bootstrap 4 JS below) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"/>
-<<<<<<< HEAD
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-=======
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
->>>>>>> d68c145659d780cdd6723abac98acad58e1d3bf7
   </head>
 
 
@@ -47,7 +43,7 @@
                     <img class="d-block w-100" src="assets/Imagenes/Inta1.jpg" alt="First slide">
                     <div class="carousel-caption d-none d-md-block">
                       <h5>AgroTech 4.0</h5>
-                      <p>Sistema web innovador diseñado para transformar la gestión de los CDTs de la Dirección de la Costa Caribe Sur del INTA, integrando principios y herramientas de la Cuarta Revolución Industrial en el ámbito agropecuario.<</p>
+                      <p>Sistema web innovador diseñado para transformar la gestión de los CDTs de la Dirección de la Costa Caribe Sur del INTA, integrando principios y herramientas de la Cuarta Revolución Industrial en el ámbito agropecuario.</p>
                     </div>
                   </div>
                   <div class="carousel-item">
@@ -128,9 +124,10 @@
 
 
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> 
-    <script src="assets/js/login.js"></script>
+  <!-- jQuery -> Popper (v1) -> Bootstrap 4 (local) -> custom login JS -->
+  <script src="Assets/jquery-3.3.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
+  <script src="Assets/js/p_bootstrap.min.js"></script>
+  <script src="Assets/js/login.js"></script>
   </body>
 </html>
