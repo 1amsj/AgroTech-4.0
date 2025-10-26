081bf7a1c0375768aa0bea2f5b208be123029eb0
legan33333dhiro <jesus2026bbb@gmail.com>
2025-10-26 19:21:16 -0400

    Remove outdated Bootstrap 4 scripts and update to Bootstrap 5 in login page

--- Vista/login.php
+++ Vista/login.php
@@ -126,19 +126,11 @@
       </div>
     -->
 
-<<<<<<< HEAD
-     
-    <!-- jQuery -> Popper -> Bootstrap 4 (local) -> custom login JS -->
-    <script src="Assets/jquery-3.3.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
-    <script src="Assets/js/p_bootstrap.min.js"></script>
-=======
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> 
->>>>>>> d68c145659d780cdd6723abac98acad58e1d3bf7
     <script src="assets/js/login.js"></script>
   </body>
 </html>
